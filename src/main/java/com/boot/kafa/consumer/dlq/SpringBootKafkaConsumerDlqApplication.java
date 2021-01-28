ca253f47ac03ba0c7d590fa6c297a528984d0311
phantasmicmeans <phantasmicmeans@kakao.com>
2021-01-28 18:40:18 +0900

    initial commit

---
+++ src/main/java/com/boot/kafa/consumer/dlq/SpringBootKafkaConsumerDlqApplication.java
@@ -0,0 +1,13 @@
+package com.boot.kafa.consumer.dlq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumerDlqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumerDlqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/kafa/consumer/dlq/SpringBootKafkaConsumerDlqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.kafa.consumer.dlq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaConsumerDlqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
